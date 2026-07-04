71fe8cc35c3b6c2d765f75e15f3283b8b5eeb2f2
mehdiking <mehdi.mohseni2002@gmail.com>
2026-07-05 00:31:06 +0330

    update static media

--- config/settings.py
+++ config/settings.py
@@ -113,9 +113,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL='/static/'
-STATIC_ROOT=BASE_DIR /'static'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-
 STATIC_URL = 'static/'
+STATIC_ROOT=BASE_DIR /'static'
--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from blog import views
 from blog.views import http_test, json_test
 from django.http import HttpResponse , JsonResponse
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
 
@@ -17,3 +19,5 @@ urlpatterns = [
     path('index/', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
