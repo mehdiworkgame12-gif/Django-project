7164f7998345c5134565355402ef09db565d4099
mehdiking <mehdi.mohseni2002@gmail.com>
2026-07-07 23:10:41 +0330

    update website

--- blog/models.py
+++ blog/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title =models.CharField()
-    content=models.TextField()
+    title= models.CharField(max_length=255)
+    content= models.TextField()
+    counted_view=models.IntegerField(default=0)
+    status=models.BooleanField(default=False)
+    created_date=models.DateTimeField(auto_now_add=True)
+    updated_date=models.DateTimeField(auto_now=True)
+    published_date=models.DateTimeField(null=True)
