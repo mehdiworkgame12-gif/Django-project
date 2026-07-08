9f527d9c1c0ad019d7a4a679b81842d5691c10c1
mehdiking <mehdi.mohseni2002@gmail.com>
2026-07-08 19:24:49 +0330

    update admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-
-# Register your models here.
 from blog.models import Post
+# Register your models here.
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy= 'created_date'
+    empty_value_display= '-empty-'
+    list_display = ('title','counted_view','status','published_date')
+    list_filter = ('status',)
+    ordering = ['-created_date']
+    search_fields = ['tirle','content']
+admin.site.register(Post,PostAdmin)
