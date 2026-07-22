cbd5e9825bfcdcc902b7424093a38f9943e81d64
mehdiking <mehdi.mohseni2002@gmail.com>
2026-07-22 22:11:28 +0330

    login server

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login_view(request):
+    if request.user.is_authenticated:
+        msg='user is already logged in'
+    else:
+        msg=None
+    return render(request,'accounts/login.html', {'msg': msg})
+
+def logout_view(request):
+    return render(request,'accounts/logout.html')
+
+def singup_view(request):
+    return render(request,'accounts/singup.html')
